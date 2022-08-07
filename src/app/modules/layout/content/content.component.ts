e3e884cb70b8c165fb0a590a49ebfc03c7cf8272
sushrutmahajan <sushrutmech@gmail.com>
2022-08-07 15:12:22 +0530

    spelling changess and add new dynamic message for goal

--- src/app/modules/layout/content/content.component.ts
+++ src/app/modules/layout/content/content.component.ts
@@ -52,6 +52,9 @@ export class ContentComponent implements OnInit, OnDestroy {
   teamGoalList: any = []
   MyGoalList: any = []
   BusinessGoalList: any = []
+  MyGoalListLength:any=[]
+
+  renderGoalTabMessage:boolean=false;
 
   end: any;
 
@@ -203,13 +206,18 @@ this.timerCourse= setInterval(() => {
     this.getAllContent();
     this.homeContentLocalStorageFun()
 
-
     this.searchForm = this.fb.group({
       searchInput: '',
       excludeSwitch: false
     })
+    console.log("*-*-", this.MyGoalList, this.MyGoalList.length)
+    //console.log("*-*-", this.allGoalData, this.allGoalData.length)
+    console.log("*-*-", this.teamGoalList, this.teamGoalList.length)
+    console.log("*-*-", this.BusinessGoalList, this.BusinessGoalList.length)
 
-
+    
+    console.log("/*/*/*", this.allGoalData)
+    
   }
 
   ngOnDestroy(): void {
@@ -218,6 +226,10 @@ this.timerCourse= setInterval(() => {
     if (this.timeSubscription) {
       this.timeSubscription.unsubscribe();
     }
+  }
+
+  ngAfterViewChecked(){
+   
   }
 
   homeContentLocalStorageFun() {
@@ -323,15 +335,10 @@ this.timerCourse= setInterval(() => {
     //this.spinner.show();
     this.myKeysToSuccessService.getGoalList().subscribe(results => {
       this.allGoalData = results
-
-      //console.log("**++--//", this.allGoalData)
-
       this.teamGoalList = this.allGoalData.filter(
         (ele: any) => { return ele.icon == "iVBORw0KGgoAAAANSUhEUgAAACUAAAAlCAMAAADyQNAxAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAA/UExURQAAAN/f397e3tfX19jY2Nra2tnZ2dfX19nZ2djY2NfX19jY2NfX19ra2tra2tnZ2dra2tnZ2dnZ2dnZ2dnZ2d4gwvcAAAAUdFJOUwAQHyAuMD1AUFtgcICPn6+/z9/vYpxEMwAAAAlwSFlzAAAOwwAADsMBx2+oZAAAAUNJREFUOE/tk8lyxCAMRK04BDJsBvT/35rWgjM19ySXdM3hldRAIzPHn4lSdAr5NKD4MLh1TuahlBezdqkzr6C1LVSYM4BgYibQQ6Brd+uSUgWQAMuZRcD238pS0mQDMAVOKRWhb5U55EA0+7osfhqryNFPouA3AzkctEuu0CV0xcooJw5ckqqUfFuVFqD1Vg242V0hywElL3DVe6na9vMOcPeSDkS17qX7mhdLGOgdkRTgBhstM2GQttvA+qbUJ2fzo+NzwVFaqZ2H+XuVnyju/I1H0EqaO39PvD6VwvRviZ65PmB0F1JEnUWoHmxyNBfxJHfhRpYiBHfhk5irhOBTwSHRCL17/OZi0O06bPjPzxCughmACk/MwFwNS15cJ3KACh5ec9cDT+zf9dMuKoXOLM8tlnzEnEClBMr55W/7SzqOL4sFKnpMQsP8AAAAAElFTkSuQmCC" }
       )
-
-      // console.log("**++--//filterTeamGoal", this.teamGoalList)
-
+      this.isDataLoaded = true;
     })
 
     return this.teamGoalList
@@ -341,19 +348,14 @@ this.timerCourse= setInterval(() => {
   getMyGoalList() {
     this.myKeysToSuccessService.getGoalList().subscribe(results => {
       this.allGoalData = results
-
-      // console.log("**++--//", this.allGoalData)
-
       this.MyGoalList = this.allGoalData.filter(
         (ele: any) => { return ele.icon == "iVBORw0KGgoAAAANSUhEUgAAACYAAAAlCAMAAAAZd2syAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAzUExURQAAAN/f39fX19ra2tfX19nZ2dfX19jY2NfX19ra2tra2tnZ2dra2tnZ2dnZ2dnZ2dnZ2VMHktcAAAAQdFJOUwAQIDBAUGBwgI+fr7/P3+8jGoKKAAAACXBIWXMAAA7DAAAOwwHHb6hkAAABUUlEQVQ4T9VT3daEIAgU0yzzh/d/2m8gas09u9/1zkVHbGAGKPeDoLR5O35DYebFzp+xgMWbBZ9BQlstmBD2fPsJre/kVJae4r4zdzsDFHOVinRwsitFEqFgQahIEuRjUhfb3VRXI6GetUJRnstKbq1VziHGrXNTWoLEWStxwQPZZVfFqO+bhxjzcTcAray2BMgkqVO8WyUeWtolvhy/mtfR8bAxLKaeUhCDV5t+lnjYBOVCKtvxPIclt2pu2oQZ1gEUwkXQVrscXyC1Fp0HSewRDHekTttXa5p69rDBbtjuaV/Q7psFwC45jbOFF87u6204ycQqzE4QFRDVPQATHiWH+obKbQFxPyNUD0gdlmDAzlZwLzfYupR8zkPQUKnctILpIPP54QK4iwNNB1PngWhvNNIU75qHdD/T3oBt4cv9l+Z1MaFfv8wnpMdf9jtw7g+gCBMfF5RfCQAAAABJRU5ErkJggg==" }
       )
-
-      //console.log("**++--//filterMyGoalList", this.MyGoalList)
-
+      this.isDataLoaded = true;
+      console.log("/*/*/*++++---", this.MyGoalListLength , this.MyGoalListLength.length)
     })
-
+    
     return this.MyGoalList
-
   }
 
 
@@ -367,9 +369,7 @@ this.timerCourse= setInterval(() => {
       this.BusinessGoalList = this.allGoalData.filter(
         (ele: any) => { return ele.icon == "iVBORw0KGgoAAAANSUhEUgAAACYAAAAlCAMAAAAZd2syAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAzUExURQAAAN/f39fX19ra2tfX19nZ2dfX19jY2NfX19ra2tra2tnZ2dra2tnZ2dnZ2dnZ2dnZ2VMHktcAAAAQdFJOUwAQIDBAUGBwgI+fr7/P3+8jGoKKAAAACXBIWXMAAA7DAAAOwwHHb6hkAAAAj0lEQVQ4T+XRQQ6EIAwF0FJRGUDo/U87k/ZjTECXJmTeqm1+rFaaxxKySTsmI1zllDAb2KV+zCHiMOwFKai8CKPsPcc4YFU+l0aRiHJDijLe+kb7aLR34myxtP0UNKrFliu9KqMxGiJarzTm0aj2O/Bwo8ccLkVrKpNbUZsXDtL7v1hAP1Y8YkTuCTJTI/oCJP0pPY2k3a4AAAAASUVORK5CYII=" }
       )
-
-      // console.log("**++--//filterBuinessGoalList", this.BusinessGoalList)
-
+      this.isDataLoaded = true;
     })
 
     return this.BusinessGoalList
